f48503e246bc8c0b677550fd0e315acecc723aef
Brian Heilman <brian.heilman@gmail.com>
2022-07-10 21:38:44 -0400

    iterating

--- packages/bmoor-path/src/parser.ts
+++ packages/bmoor-path/src/parser.ts
@@ -40,7 +40,7 @@ export class DotPattern {
 
 	toToken(base: string, state: TokenizerState) {
 		return new ValueToken(
-			this.parser(base.substring(state.open, state.close + 1)),
+			this.parser(base.substring(state.open, state.close)),
 			state,
 			{
 				subType: this.subType
@@ -50,7 +50,35 @@ export class DotPattern {
 }
 
 export class BracketPattern {
+	open(str: string, pos: number) {
+		const ch = str[pos];
 
+		if (ch === '[') {
+			return new TokenizerState(pos);
+		}
+
+		return null;
+	}
+
+	close(str: string, pos: number, state: TokenizerState) {
+		const ch = str[pos];
+
+		if (ch === ']') {
+			return pos;
+		}
+
+		return null;
+	}
+
+	toToken(base: string, state: TokenizerState) {
+		return new ArrayToken(
+			this.parser(base.substring(state.open, state.close)),
+			state,
+			{
+				subType: this.subType
+			}
+		);
+	}
 }
 
 export class Parser extends Compiler {
