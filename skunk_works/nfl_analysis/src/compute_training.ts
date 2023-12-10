1ef7c5094fc068aaa02305dce050bdcd0edfd6ce
Brian Heilman <brian.heilman@gmail.com>
2023-12-10 14:00:04 -0500

    fix: don't spam promises

--- skunk_works/nfl_analysis/src/compute_training.ts
+++ skunk_works/nfl_analysis/src/compute_training.ts
@@ -1,20 +1,28 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
+console.log('fresh', process.memoryUsage());
 import {graph} from './features';
 import {calculateCompare} from './compute';
+console.log('loaded', process.memoryUsage());
 
 async function run(){
-    const rtn = [];
+    const rtn = {
+        keys: null,
+        training: []
+    };
 
-    for (const interval of graph.intervals.values()){
+    for (const interval of Array.from(graph.intervals.values()).slice(8, 40)){
         console.log(interval);
+        console.log(process.memoryUsage());
         const weekGraph = graph.getGraph(interval);
+
+        const proc = [];
 
         for (const event of weekGraph.eventDex.values()){
             const nodes = event.getNodesByType('team');
 
-            rtn.push(
+            proc.push(
                 calculateCompare(interval.ref, nodes[0].ref, nodes[1].ref)
                 .then((res: Record<string, number>[]) => {
                     const keys = Object.keys(res[0]).slice(2);
@@ -39,6 +47,16 @@ async function run(){
                 })
             );
         }
+
+        await Promise.all(proc).then(rows => {
+            for (const row of rows){
+                if (!rtn.keys){
+                    rtn.keys = row.metadata.keys
+                }
+
+                rtn.training.push([row.compare, row.label]);
+            }
+        });
     }
 
     fs.writeFileSync(
